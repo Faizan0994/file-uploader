6dda343f3d3daec5edc718cae99eece7c7933769
faizan <faizanamjad575@gmail.com>
2025-10-12 17:53:25 +0500

    set url encoded

--- app.js
+++ app.js
@@ -9,11 +9,11 @@ const PORT = process.env.PORT || 3000;
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.render("index");
 });
-
 app.use("/auth", authRouter);
 app.use("/users", usersRouter);
 
