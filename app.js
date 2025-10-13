4c33c742f565292a0e14938f6b627e1ef49f9487
faizan <faizanamjad575@gmail.com>
2025-10-13 11:41:12 +0500

    implement express session with prisma

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const express = require("express");
 const path = require("path");
 const authRouter = require("./routes/auth");
 const usersRouter = require("./routes/users");
+const session = require("express-session");
+const { PrismaSessionStore } = require("@quixo3/prisma-session-store");
+const client = require("./database/client");
+require("dotenv").config();
 
 const app = express();
 
@@ -10,6 +14,21 @@ const PORT = process.env.PORT || 3000;
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    saveUninitialized: false,
+    resave: false,
+    cookie: {
+      maxAge: 2 * 24 * 60 * 60 * 1000, // Two days
+    },
+    store: new PrismaSessionStore(client, {
+      checkPeriod: 2 * 60 * 1000,
+      dbRecordIdIsSessionId: true,
+      dbRecordIdFunction: undefined,
+    }),
+  })
+);
 
 app.get("/", (req, res) => {
   res.render("index");
