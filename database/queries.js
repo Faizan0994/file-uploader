6a35f43c82e15fe1604ecc2156a00ec9ffd1d809
faizan <faizanamjad575@gmail.com>
2025-10-13 10:18:30 +0500

    add queries to get user by username and id

--- database/queries.js
+++ database/queries.js
@@ -13,3 +13,13 @@ exports.registerUser = async (name, username, password) => {
 exports.getAllUsers = async () => {
   return await prisma.user.findMany();
 };
+
+exports.getUserByUsername = async (username) => {
+  const user = await prisma.user.findUnique({ where: { username: username } });
+  return user;
+};
+
+exports.getUserById = async (id) => {
+  const user = await prisma.user.findUnique({ where: { id: id } });
+  return user;
+};
