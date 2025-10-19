8c35d86fdf6ea77ad593409aca21c7f04916e653
faizan <faizanamjad575@gmail.com>
2025-10-19 18:27:43 +0500

    add functionality to upload files via multer middleware

--- controllers/users-controller.js
+++ controllers/users-controller.js
@@ -1,5 +1,25 @@
 const express = require("express");
+const multer = require("multer");
+const path = require("path");
 
+// Storage handling
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, path.resolve("./tmp/uploads"));
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    cb(null, file.fieldname + "-" + uniqueSuffix + file.originalname);
+  },
+});
+const upload = multer({
+  storage: storage,
+  limits: {
+    fileSize: 4096, // 4 MB
+  },
+});
+
+// Controllers
 exports.dashboardGet = (req, res) => {
   let user = req.user;
   if (user) {
@@ -14,3 +34,10 @@ exports.uploadGet = (req, res) => {
 
   res.render("upload");
 };
+
+exports.uploadPost = [
+  upload.single("uploadedFile"),
+  (req, res) => {
+    res.redirect("/users/dashboard");
+  },
+];
